1855d0b5a5df903c866352be409c0eed6eefdfe1
Breyden Taylor <124335295+prompted365@users.noreply.github.com>
2025-01-07 23:53:08 -0500

    removed non-root users

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -1,5 +1,3 @@
-# tasks/celery_app.py
-
 import os
 import ssl
 from celery import Celery
@@ -7,14 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Upstash Redis broker URL. Example format:
-# rediss://:<PASSWORD>@<UPSTASH_REDIS_ENDPOINT>:<PORT>
-#
-# In production, store this securely (e.g., in an .env file or secret manager)
-BROKER_URL = os.getenv("UPSTASH_BROKER_URL")
-
-# We can use the same URL for the backend, or omit "backend="
-# if we do not need result tracking.
+BROKER_URL = os.getenv("UPSTASH_REDIS_URL") or os.getenv("REDIS_URL")
 BACKEND_URL = BROKER_URL
 
 celery_app = Celery("Sunlink Celery", broker=BROKER_URL, backend=BACKEND_URL)
@@ -27,8 +18,6 @@ celery_app.conf.update(
     redis_backend_use_ssl={
         "ssl_cert_reqs": ssl.CERT_NONE
     },
-    # Add this line to explicitly include your module
     include=["tasks.worker_tasks"],
     broker_connection_retry_on_startup=True
 )
-
