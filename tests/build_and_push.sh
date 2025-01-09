586fb567c076f55d5de5adea18d70a899bd469b0
Breyden Taylor <124335295+prompted365@users.noreply.github.com>
2025-01-09 04:23:29 -0500

    scripts

---
+++ tests/build_and_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# GCP Project ID
+PROJECT_ID="solarflux-harmony"
+
+# Build and push the Worker image
+docker buildx build \
+  --platform=linux/amd64,linux/arm64 \
+  -t gcr.io/${PROJECT_ID}/sunlink-worker:latest \
+  -f Dockerfile.worker \
+  --push
+
+# Build and push the API image
+docker buildx build \
+  --platform=linux/amd64,linux/arm64 \
+  -t gcr.io/${PROJECT_ID}/sunlink-api:latest \
+  -f Dockerfile.api \
+  --push
+
+echo "Docker images pushed to GCR successfully!"
