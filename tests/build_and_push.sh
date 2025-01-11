97d8312f4050ea049a237646ca12ea0cb4dd6554
Breyden Taylor <124335295+prompted365@users.noreply.github.com>
2025-01-11 00:59:23 -0500

    updated shell script to build and push docker images to my artifacts registry.

--- tests/build_and_push.sh
+++ tests/build_and_push.sh
@@ -6,14 +6,14 @@ PROJECT_ID="solarflux-harmony"
 # Build and push the Worker image
 docker buildx build \
   --platform=linux/amd64,linux/arm64 \
-  -t gcr.io/${PROJECT_ID}/sunlink-worker:latest \
+  -t sunlinkai-artifacts/${PROJECT_ID}/sunlink-worker:latest \
   -f Dockerfile.worker \
   --push
 
 # Build and push the API image
 docker buildx build \
   --platform=linux/amd64,linux/arm64 \
-  -t gcr.io/${PROJECT_ID}/sunlink-api:latest \
+  -t sunlinkai-artifacts/${PROJECT_ID}/sunlink-api:latest \
   -f Dockerfile.api \
   --push
 
